2425f4734e7cf268b5b6e27f1a0fbedd9fceac86
Sasha Makarava <4079864+SashaMkrv@users.noreply.github.com>
2024-10-01 14:07:58 -0400

    update image save behaviour
    Web uses the JS bridge to save in the expected place. Great! OS saves to the user data. Not so great! It is very inconvenient to get to them.

--- Scripts/ImageSaver.gd
+++ Scripts/ImageSaver.gd
@@ -5,7 +5,16 @@ func trySaveImage(image: Image) -> void:
 	imageCopy.resize(500, 500, Image.INTERPOLATE_NEAREST)
 	
 	var time = str(Time.get_unix_time_from_system()).replace(".", "-")
+	
+	match OS.get_name():
+		"Web":
+			var buffer = imageCopy.save_png_to_buffer()
+			JavaScriptBridge.download_buffer(buffer, "conway-" + time +".png", "image/png")
+		_:
+			imageCopy.convert(Image.FORMAT_RGB8)
+			var imagePath = "user://conway-" + time + ".png"
+			#var imagePath = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) \
+			#+ "/" + "game_of_life_" + time + ".png"
+			imageCopy.save_png(imagePath)
 		
-	var imagePath = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES) \
-		+ "/" + "game_of_life_" + time + ".png"
-	imageCopy.save_png(imagePath)
+	
